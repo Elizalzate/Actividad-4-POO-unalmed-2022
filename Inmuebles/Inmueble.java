a6d73359f7a2a7b330940639ff18c64629a81e41
Elízabeth Alzate <elizas95@gmail.com>
2022-10-20 12:49:35 -0500

    packages

---
+++ Inmuebles/Inmueble.java
@@ -0,0 +1,26 @@
+package Inmuebles;
+
+public class Inmueble {
+    protected  int identificadorInmobiliario;
+    protected  float area;
+    protected  String direccion;
+    protected  double precioVenta;
+
+    Inmueble(int identificadorInmobiliario, float area, String direccion){
+        this.identificadorInmobiliario = identificadorInmobiliario;
+        this.direccion = direccion;
+        this.area = area;
+    }
+
+    protected double calcularPrecioVenta(double valorArea){
+        precioVenta = area * valorArea;
+        return  precioVenta;
+    }
+
+    protected void imprimir(){
+        System.out.println("Identificador inmobiliario: " + identificadorInmobiliario +
+                "\nÁrea del inmueble: " + area + "\nDirección: " + direccion +
+                "\nPrecio de venta: $" + precioVenta);
+    }
+
+}
