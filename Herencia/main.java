c0da72a1d2bdf03e0619f9a7246f347fc22a67ae
Elízabeth Alzate <elizas95@gmail.com>
2022-10-20 12:42:25 -0500

    Herencia cuenta

--- Herencia/main.java
+++ Herencia/main.java
@@ -1,9 +1,9 @@
 public class main {
     public static void main(String[] args) {
         Cuenta cuenta = new Cuenta(100, 10);
-        cuenta.consultar();
+        cuenta.imprimir();
         cuenta.consignar(100);
-        cuenta.consultar();
+        cuenta.imprimir();
         cuenta.retirar(300);
     }
 }
