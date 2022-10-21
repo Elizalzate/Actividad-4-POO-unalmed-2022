c81fea63c5e867507f3f19ac8dffbd7e1afe1b4d
Elízabeth Alzate <elizas95@gmail.com>
2022-10-20 23:45:43 -0500

    Abstract classes and intefaces

--- Animales/Animal.java
+++ Animales/Animal.java
@@ -1,9 +1,48 @@
 package Animales;
 
-public class Animal {
+public class Animal implements IAnimal{
     protected String sonido;
     protected String alimentos;
     protected String habitat;
     protected String nombreCientifico;
 
+    @Override
+    public void setNombreCientifico(String nombre) {
+        this.nombreCientifico = nombre;
+    }
+
+    @Override
+    public void setSonido(String sonido) {
+        this.sonido = sonido;
+    }
+
+    @Override
+    public void setAlimentos(String alimento) {
+        this.alimentos = alimento;
+    }
+
+    @Override
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    @Override
+    public String getSonido() {
+        return sonido;
+    }
+
+    @Override
+    public String getAlimentos() {
+        return alimentos;
+    }
+
+    @Override
+    public String getHabitat() {
+        return habitat;
+    }
 }
--- Animales/IAnimal.java
+++ Animales/IAnimal.java
@@ -1,11 +1,17 @@
 package Animales;
 
 interface IAnimal {
+
+    void setNombreCientifico(String nombre);
+
+    void setSonido(String sonido);
+    void setAlimentos(String alimento);
+    void setHabitat(String habitat);
+
+
+
     public String getNombreCientifico();
-
     public String getSonido();
-
     public String getAlimentos();
-
     public String getHabitat();
 }
