a9eb058f7cc960c78a4cdec6757132ae350cdd6e
Elízabeth Alzate <elizas95@gmail.com>
2022-10-20 19:27:08 -0500

    Abstract classes and intefaces

---
+++ Animales/Animal.java
@@ -0,0 +1,9 @@
+package Animales;
+
+public class Animal {
+    protected String sonido;
+    protected String alimentos;
+    protected String habitat;
+    protected String nombreCientifico;
+
+}
---
+++ Animales/IAnimal.java
@@ -0,0 +1,11 @@
+package Animales;
+
+interface IAnimal {
+    public String getNombreCientifico();
+
+    public String getSonido();
+
+    public String getAlimentos();
+
+    public String getHabitat();
+}
