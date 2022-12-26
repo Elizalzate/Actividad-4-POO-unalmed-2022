d4f882408ea08af54de52581e8e86537608d2931
crrojasmazo <71391747+crrojasmazo@users.noreply.github.com>
2022-12-25 21:36:05 -0500

    leon a leonsito

--- Animales/Leon.java
+++ Animales/Leon.java
@@ -11,7 +11,7 @@ public class Leon extends Felino {
 
     void imprimirInfo() {
         System.out.println("\n" + getNombreCientifico() +
-                " también conocido como León" +
+                " también conocido como Leónsito" +
                 "\nSonido: " + getSonido() +
                 "\nSu habitat es: " + getHabitat() +
                 "\nSu alimentación está basada en: " + getAlimentos());
